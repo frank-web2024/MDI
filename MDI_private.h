6fbe33f277a3d9e7b40415eb0ea9e877e1166008
frank-web2024 <aghostfrank@hotmail.com>
2024-09-17 16:10:25 +0800

    Initial commit

---
+++ MDI_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef MDI_PRIVATE_H
+#define MDI_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.1.0.0"
+#define VER_MAJOR	1
+#define VER_MINOR	1
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"Ghost Studio 2024"
+#define FILE_VERSION	"1.1.0.0"
+#define FILE_DESCRIPTION	"MDI Editor"
+#define INTERNAL_NAME	"Ghost Studio 2024"
+#define LEGAL_COPYRIGHT	"Ghost Studio 2024"
+#define LEGAL_TRADEMARKS	"Ghost Studio 2024"
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"MDI Editor"
+#define PRODUCT_VERSION	"1.1.0.0"
+
+#endif /*MDI_PRIVATE_H*/
